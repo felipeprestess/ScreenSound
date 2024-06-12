9596e3bf9245d290880e30cfaabfc6630004edea
Felipe . <felipeadriano_pc@hotmail.com>
2024-06-11 22:20:06 -0300

    Add project files.

---
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Data.SqlClient;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco
+{
+    public class ArtistaDAL
+    {
+        public IEnumerable<Artista> Listar()
+        {
+            var lista = new List<Artista>();
+            using var connection = new Connection().ObterConexao();
+            connection.Open();
+
+            string query = "SELECT * FROM Artistas";
+            var command = new SqlCommand(query, connection);
+            using SqlDataReader dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                string nomeArtista = Convert.ToString(dataReader["Nome"]);
+                string bio = Convert.ToString(dataReader["Bio"]);
+                int idArtista = Convert.ToInt32(dataReader["Id"]);
+
+                Artista artista = new(nomeArtista, bio)
+                {
+                    Id = idArtista,
+                };
+
+                lista.Add(artista);
+            }
+
+            return lista;
+        }
+
+        public void Adicionar(Artista artista)
+        {
+            using var connection = new Connection().ObterConexao();
+            connection.Open();
+
+            string sql = "INSERT INTO Artistas (Nome, FotoPerfil, Bio) VALUES (@nome, @perfilPadrao, @bio)";
+
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@nome", artista.Nome);
+            command.Parameters.AddWithValue("@perfilPadrao", artista.FotoPerfil);
+            command.Parameters.AddWithValue("@bio", artista.Bio);
+
+            int retorno = command.ExecuteNonQuery();
+
+            Console.WriteLine($"Linhas afetadas: {retorno}");
+        }
+
+        public void Atualizar(Artista artista)
+        {
+            using var connection = new Connection().ObterConexao();
+            connection.Open();
+
+            string sql = "UPDATE Artistas SET Nome = @nome, Bio = @bio WHERE Id = @id";
+            var command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@nome", artista.Nome);
+            command.Parameters.AddWithValue("@bio", artista.Bio);
+            command.Parameters.AddWithValue("@id", artista.Id);
+
+            int retorno = command.ExecuteNonQuery();
+
+            Console.WriteLine($"Linhas afetadas: {retorno}");
+        }
+
+        public void Deletar(int id)
+        {
+            using var connection = new Connection().ObterConexao();
+            connection.Open();
+
+            string sql = "DELETE FROM Artistas WHERE Id = @id";
+
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+
+            int retorno = command.ExecuteNonQuery();
+
+            Console.WriteLine($"Linhas afetadas: {retorno}");
+        }
+    }
+}
---
+++ ScreenSound/Banco/Connection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ScreenSound.Banco;
+
+public class Connection
+{
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundDb;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    public SqlConnection ObterConexao()
+    {
+        return new SqlConnection(connectionString);
+    }
+}
