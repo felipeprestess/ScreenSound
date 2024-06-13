86c4aecc8b1545354794467e6ab163f44ed16a7b
Felipe . <felipeadriano_pc@hotmail.com>
2024-06-12 22:27:09 -0300

    Adicionando generics

---
+++ ScreenSound/Banco/DAL.cs
@@ -0,0 +1,12 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco
+{
+    public abstract class DAL<T>
+    {
+        public abstract IEnumerable<T> Listar();
+        public abstract void Adicionar(T objeto);
+        public abstract void Atualizar(T objeto);
+        public abstract void Deletar(T objeto);
+    }
+}
---
+++ ScreenSound/Banco/MusicaDAL.cs
@@ -0,0 +1,43 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco
+{
+    internal class MusicaDAL : DAL<Musica>
+    {
+        private readonly ScreenSoundContext _context;
+
+        public MusicaDAL(ScreenSoundContext context)
+        {
+            _context = context;
+        }
+
+        public override IEnumerable<Musica> Listar()
+        {
+            return _context.Musicas.ToList();
+        }
+
+        public override void Adicionar(Musica musica)
+        {
+            _context.Musicas.Add(musica);
+
+            _context.SaveChanges();
+        }
+
+        public override void Atualizar(Musica musica)
+        {
+            _context.Musicas.Update(musica);
+            _context.SaveChanges();
+        }
+
+        public override void Deletar(Musica musica)
+        {
+            _context.Remove(musica);
+            _context.SaveChanges();
+        }
+
+        public Musica? RecuperarPeloNome(string nome)
+        {
+            return _context.Musicas.FirstOrDefault(x => x.Nome.Equals(nome));
+        }
+    }
+}
